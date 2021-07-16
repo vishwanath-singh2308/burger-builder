122e78d1e88e0327fad50ff62c6b0dd2f718de17
vishwanath-singh2308 <vishwanathsingh.2308@gmail.com>
2021-07-16 19:36:56 +0530

    Redux advanced

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -181,7 +181,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return{
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger)
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
     }
 }
 
