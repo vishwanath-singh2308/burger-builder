94aac2f6264662aea6756df0e8422266a9d35c39
vishwanath-singh2308 <vishwanathsingh.2308@gmail.com>
2021-07-25 15:57:18 +0530

    update 1.0

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -31,7 +31,7 @@ const removeIngredient = (state,action) => {
             const updatedIngs = updateObject(state.ingredients,updatedIng)
             const updatedSt = {
                 ingredients: updatedIngs,
-                totalPrice: state.totalPrice + INGREDIENT_PRICE[action.ingredientName],
+                totalPrice: state.totalPrice - INGREDIENT_PRICE[action.ingredientName],
                 building: true 
             }
             return updateObject(state, updatedSt)
